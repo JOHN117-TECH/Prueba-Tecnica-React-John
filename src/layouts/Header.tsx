4b49a1890173cfde5046117f3f4065606a28353c
John Barreto <barretodiazjohnalexander@gmail.com>
2025-11-19 15:28:21 -0500

    add: icons to header items

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -26,6 +26,17 @@ const Header = () => {
                 isActive ? 'btn btn-primary' : 'btn'
               }
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="m16 8.41l-4.5-4.5L4.41 11H6v8h3v-6h5v6h3v-8h1.59L17 9.41V6h-1zM2 12l9.5-9.5L15 6V5h3v4l3 3h-3v8h-5v-6h-3v6H5v-8z"
+                />
+              </svg>
               Home
             </NavLink>
           </li>
@@ -37,7 +48,20 @@ const Header = () => {
                 isActive ? 'btn btn-primary' : 'btn'
               }
             >
-              Information API
+              Information{' '}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path fill="currentColor" d="M13.26 10.5h2v1h-2z" />
+                <path
+                  fill="currentColor"
+                  d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M8.4 15L8 13.77H6.06L5.62 15H4l2.2-6h1.62L10 15Zm8.36-3.5a1.47 1.47 0 0 1-1.5 1.5h-2v2h-1.5V9h3.5a1.47 1.47 0 0 1 1.5 1.5ZM20 15h-1.5V9H20Z"
+                />
+                <path fill="currentColor" d="M6.43 12.77h1.16l-.58-1.59z" />
+              </svg>
             </NavLink>
           </li>
 
@@ -48,6 +72,23 @@ const Header = () => {
                 isActive ? 'btn btn-primary' : 'btn'
               }
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                >
+                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                  <circle cx="12" cy="7" r="4" />
+                </g>
+              </svg>{' '}
               User
             </NavLink>
           </li>
